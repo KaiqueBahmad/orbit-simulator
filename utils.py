cae9afed80b99b12dfc5b2f30a2a99c0b78543a5
Kaique Bahmad <kaiquebahmadt@gmail.com>
2024-05-10 19:33:21 -0300

    instrução para criação de função python

---
+++ utils.py
@@ -0,0 +1,11 @@
+
+
+
+#Exemplo de função para ajudar quem for escrever aqui
+def zero_filled_array(array_size): #Coloque nomes entendíveis nos argumentos
+    array = []
+    for i in range(array_size):
+        array.append(0)
+    return array #A saída NAO deve ser um print, deve ser sempre um retorno
+                 # Use prints apenas para DEBUGAR assim que o código estiver
+                 # funcionando, remova os prints!!
