a58903d21df1a6c4bdfb0994f0b4626cb073ab8a
Kaique Bahmad <kaiquebahmadt@gmail.com>
2024-05-13 01:34:39 -0300

    documentando função para calcular força entre dois corpos

--- src/utils.py
+++ src/utils.py
@@ -11,13 +11,15 @@ def zero_filled_array(array_size): #Coloque nomes entendíveis nos argumentos
                  # Use prints apenas para DEBUGAR assim que o código estiver
                  # funcionando, remova os prints!!
 
+#G stands for the gravitional constant, bigger it's more powerful gravity will be
 def forceBetween(b1:Body, b2:Body, G:float=1) -> list :
     distance:float = ( (b1.x - b2.x)**2 + (b1.y - b2.y)**2 )**(1/2)
     sin:float = abs(b2.y - b1.y) / distance
     cos:float = (1-sin**2)**(1/2)
+    #Calculates the force between the bodies
     force:float =  (b1.mass * b2.mass) / distance**2
-    force *= G
-    fx:float = force * cos
-    fy:float = force * sin
-    return [fx, fy]
+    force *= G #Applies the gravitiational constant
+    fx:float = force * cos # Decomposes the force to X
+    fy:float = force * sin # Decomposes the force to Y
+    return [fx, fy]        # The return is the decomposed forces
 
