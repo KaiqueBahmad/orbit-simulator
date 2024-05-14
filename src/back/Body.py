e6507f712694635cda069519cdf96a2362786976
KaiqueBahmad <84449201+KaiqueBahmad@users.noreply.github.com>
2024-05-14 18:07:13 -0300

    Update Body.py

--- src/back/Body.py
+++ src/back/Body.py
@@ -1,20 +1,16 @@
 class Body:
-    def __init__(self, mass, x, y, vx, vy):
+    def __init__(self, mass, x, y, vx, vy, custom_repr=False):
         self.mass = mass
         self.x = x
         self.y = y
         self. vx = vx
         self.vy = vy
+        self.custom_repr = custom_repr
 
     def __repr__(self) -> str:
-        repr_ = "\n"
-        repr_ += "-"*25
-        repr_ += f"\nMassa: {self.mass}"
-        repr_ += f"\nPosition: ({self.x}, {self.y})\n"
-        repr_ += f"Speed: ({self.vx}, {self.vy})\n"
-        repr_ += "-"*25
-        
-        return repr_
-
+        if self.custom_repr:
+            return f"{self.mass}KG ({self.x}, {self.y}) ({self.vx}, {self.vy})"
+        else:
+            return super().__repr__()
 if __name__ == '__main__':
     b1 = Body(1, 1, 1, 1, 1)
