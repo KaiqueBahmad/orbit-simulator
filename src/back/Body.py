926678ab77bca99c56bb51a32e7e0c01e82df9ce
Vitimip <106410073+Vitimip@users.noreply.github.com>
2024-05-17 19:37:44 -0300

    added get e set basico

--- src/back/Body.py
+++ src/back/Body.py
@@ -7,6 +7,22 @@ class Body:
         self.vy = vy
         self.custom_repr = custom_repr
 
+    @property
+    def position(self, x , y):
+        return self._position
+    
+    @position.setter
+    def position(self, values):
+        self._position = values
+        
+    @property
+    def speed(self, vx , vy):
+        return self._speed
+    
+    @property
+    def mass(self):
+        return self._mass
+    
     def __repr__(self) -> str:
         if self.custom_repr:
             return f"{self.mass}KG ({self.x}, {self.y}) ({self.vx}, {self.vy})"
