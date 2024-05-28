10ad882f4bb0c5ea983cdeb79d7eed2f6e89df63
Kaique Bahmad <kaiquebahmadt@gmail.com>
2024-05-27 22:08:22 -0300

    loop do sender refatorado para nao dar conflito com o backend

--- src/front/Receiver.py
+++ src/front/Receiver.py
@@ -22,28 +22,47 @@ class Receiver(QRunnable):
     def run(self):
         loop = True
         while loop:
-            with open('src/mailbox','r+') as file:
-                file.seek(0)
-                content = file.readlines()
-                self.boxStatus = content[0].strip()
-                
-                if self.boxStatus == '0':
-                    #os.system(clear)
-                    #currentId = content[1].strip + 1
-                    
-                    print(content)
-                    data = []
+            if self.firstBit() == '0':
+                data = []
+                with open('src/mailbox','r+', encoding='utf-8') as file:
+                    file.seek(0)
+                    content = file.readlines()
                     data = content[2]
                     dataDict = json.loads(data)
                     for element in dataDict:
                         planets.append(element)
-                        
-                    content[0] = '1'
-                    print(content)
                     file.seek(0)
-                    file.write("\n".join(content))
-                continue
+                    file.write("1")
+            
+            # with open('src/mailbox','r+') as file:
+            #     file.seek(0)
+            #     content = file.readlines()
+            #     self.boxStatus = content[0].strip()
+                
+            #     if self.boxStatus == '0':
+            #         #os.system(clear)
+            #         #currentId = content[1].strip + 1
+                    
+            #         print(content)
+                    # data = []
+                    # data = content[2]
+                    # dataDict = json.loads(data)
+                    # for element in dataDict:
+                    #     planets.append(element)
+                        
+                    # content[0] = '1'
+                    # print(content)
+                    # file.seek(0)
+                    # file.write("\n".join(content))
+            #     continue
 
+    def firstBit(self):
+        try:
+            with open("../mailbox", 'r', encoding="utf-8") as file:
+                return file.read(1)
+        except:
+            return 0
+    
     def storePlanet(self,obj):
         planets.append(obj)
         self.boxStatus = '0'
