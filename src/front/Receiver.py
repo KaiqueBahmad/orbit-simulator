44edd25543eacf1edc93aecdbf06798381b60522
Kaique Bahmad <kaiquebahmadt@gmail.com>
2024-05-27 22:11:51 -0300

    removendo codigo comentado, e ajustando retornos da funcao first bit

--- src/front/Receiver.py
+++ src/front/Receiver.py
@@ -33,45 +33,15 @@ class Receiver(QRunnable):
                         planets.append(element)
                     file.seek(0)
                     file.write("1")
-            
-            # with open('src/mailbox','r+') as file:
-            #     file.seek(0)
-            #     content = file.readlines()
-            #     self.boxStatus = content[0].strip()
-                
-            #     if self.boxStatus == '0':
-            #         #os.system(clear)
-            #         #currentId = content[1].strip + 1
-                    
-            #         print(content)
-                    # data = []
-                    # data = content[2]
-                    # dataDict = json.loads(data)
-                    # for element in dataDict:
-                    #     planets.append(element)
-                        
-                    # content[0] = '1'
-                    # print(content)
-                    # file.seek(0)
-                    # file.write("\n".join(content))
-            #     continue
 
     def firstBit(self):
         try:
             with open("../mailbox", 'r', encoding="utf-8") as file:
                 return file.read(1)
         except:
-            return 0
+            return 1
     
     def storePlanet(self,obj):
         planets.append(obj)
-        self.boxStatus = '0'
-       # with open('src/mailbox','w') as file:
-        #    file.write(f"0\n{currentId}\n{json.dumps(planets)}\n")
-
-                    
-                    
-                
-            
-       
+        self.boxStatus = '0'            
             
